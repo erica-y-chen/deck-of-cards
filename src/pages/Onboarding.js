aa98099ac4b1356e5a0810672f84a7c2b3cb8d8d
Erica <eyufanchen@gmail.com>
2019-07-10 14:14:16 -0700

    adding animate on scroll library for simple animations

--- src/pages/Onboarding.js
+++ src/pages/Onboarding.js
@@ -3,14 +3,23 @@ import axios from 'axios';
 import CardDeck from '../components/CardDeck.js'
 import {Clover, Diamond, Spades, Hearts} from '../components/CardDeckModern'
 import ChangePlayerModal from "../components/ChangePlayerModal.js";
+import './onboarding.css'
+import 'aos/dist/aos.css';
+
+import AOS from 'aos'
 
 class Onboarding extends Component {
  
 
   render () {
+    AOS.init();
+    
     return (
-      <div className="App">
-        hello
+      <div>
+        <div className="onboarding-header" data-aos="zoom-out-up">
+            <div className="onboarding-intro">Welcome to High-Low!</div>
+            <div className="game-description">a simple and fun game for two that will keep you guessing</div>
+        </div>
       </div>
     );
   }
