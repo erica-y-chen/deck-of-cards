898d2069db3e6e875298054d790c3bba96033531
Erica <eyufanchen@gmail.com>
2019-07-10 10:45:19 -0700

    added in a component importing modern style of cards

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import axios from 'axios';
 import CardDeck from './components/CardDeck.js'
 import logo from './logo.svg';
 import './App.css';
+import {Clover, Diamond, Spades, Hearts} from './components/CardDeckModern'
 
 class App extends Component {
   state = {
     deckID: "",
-    card: {}
+    card: [],
+    drawnCard: [],
+    playerGuess: true, 
+    correct: 0,
+    points: 0,
+    modern: true,
   };
 
   componentDidMount() {
@@ -18,18 +24,18 @@ class App extends Component {
         this.setState ({
           deckID: res.data.deck_id
         })
-        axios
-          .get(`https://deckofcardsapi.com/api/deck/${res.data.deck_id}/draw/?count=1` )
-          .then(drawn => {
-              // this.state.card.push(drawn)
-              this.setState ({
-                card: [...drawn.data.cards],
-              })
-              console.log(drawn.data.cards)}
-          )
-          .catch(err => {
-              console.log(err);
-          }) 
+        // axios
+        //   .get(`https://deckofcardsapi.com/api/deck/${res.data.deck_id}/draw/?count=1` )
+        //   .then(drawn => {
+        //       // this.state.card.push(drawn)
+        //       this.setState ({
+        //         card: [...drawn.data.cards],
+        //       })
+        //       console.log(drawn.data.cards)}
+        //   )
+        //   .catch(err => {
+        //       console.log(err);
+        //   }) 
       
       })
       .catch(err => {
@@ -38,20 +44,45 @@ class App extends Component {
   }
 
   drawCard = () => {
+    if(this.state.drawnCard) {
+      let deck=this.state.card;
+      deck.push(this.state.drawnCard)
+      this.setState ({
+        card: deck
+      })
+    }
     axios
           .get(`https://deckofcardsapi.com/api/deck/${this.state.deckID}/draw/?count=1` )
           .then(res => {
               // this.state.card.push(drawn)
-              let deck = this.state.card; 
-              deck.push(res.data.cards[0])
+              // let deck = this.state.card; 
+              // deck.push(res.data.cards[0])
               this.setState ({
-                card: deck,
+                drawnCard: res.data.cards[0],
               })
-              console.log(res.data.cards[0])}
-          )
+              console.log(res.data.cards[0])
+              
+              console.log(this.state.card[this.state.card.length-1].value)
+              this.state.drawnCard.value > this.state.card[this.state.card.length-1].value ? 
+                console.log("higher") : console.log("lower")
+              
+            })
           .catch(err => {
               console.log(err);
           }) 
+  }
+
+
+  toggleTrue = () => {
+    this.setState ({
+      playerGuess: true,
+    })
+  }
+
+  toggleFalse = () => {
+    this.setState ({
+      playerGuess: false,
+    })
   }
 
   render () {
@@ -62,7 +93,15 @@ class App extends Component {
           {this.state.deckID}
         </header>
 
+
+        <img src={Clover[1]} />
+
+
         <body>
+          drawn card: <img src={this.state.drawnCard.image} />
+          Guess: 
+          <button onClick={this.toggleTrue} className = {this.state.playerGuess ? "selected" : "unselected"} >High</button>
+          <button onClick={this.toggleFalse} className = {!this.state.playerGuess ? "selected" : "unselected"}>Low</button>
           <button onClick = {this.drawCard}>draw card</button>
           {this.state.card.length>0 ? <div className="cards">{this.state.card.map(card =>
               <img className = "card" src={card.image} />
---
+++ src/components/CardDeckModern.js
@@ -0,0 +1,74 @@
+import cardBack from '../images/card_deck_modern/2x/card-back.png'
+
+//Clover
+import clover1 from '../images/card_deck_modern/2x/clover-1.png'
+import clover2 from '../images/card_deck_modern/2x/clover-2.png'
+import clover3 from '../images/card_deck_modern/2x/clover-3.png'
+import clover4 from '../images/card_deck_modern/2x/clover-4.png'
+import clover5 from '../images/card_deck_modern/2x/clover-5.png'
+import clover6 from '../images/card_deck_modern/2x/clover-6.png'
+import clover7 from '../images/card_deck_modern/2x/clover-7.png'
+import clover8 from '../images/card_deck_modern/2x/clover-8.png'
+import clover9 from '../images/card_deck_modern/2x/clover-9.png'
+import clover10 from '../images/card_deck_modern/2x/clover-10.png'
+import clover11 from '../images/card_deck_modern/2x/clover-11.png'
+import clover12 from '../images/card_deck_modern/2x/clover-12.png'
+import clover13 from '../images/card_deck_modern/2x/clover-13.png'
+
+//Diamond
+import diamond1 from '../images/card_deck_modern/2x/diamond-1.png'
+import diamond2 from '../images/card_deck_modern/2x/diamond-2.png'
+import diamond3 from '../images/card_deck_modern/2x/diamond-3.png'
+import diamond4 from '../images/card_deck_modern/2x/diamond-4.png'
+import diamond5 from '../images/card_deck_modern/2x/diamond-5.png'
+import diamond6 from '../images/card_deck_modern/2x/diamond-6.png'
+import diamond7 from '../images/card_deck_modern/2x/diamond-7.png'
+import diamond8 from '../images/card_deck_modern/2x/diamond-8.png'
+import diamond9 from '../images/card_deck_modern/2x/diamond-9.png'
+import diamond10 from '../images/card_deck_modern/2x/diamond-10.png'
+import diamond11 from '../images/card_deck_modern/2x/diamond-11.png'
+import diamond12 from '../images/card_deck_modern/2x/diamond-12.png'
+import diamond13 from '../images/card_deck_modern/2x/diamond-13.png'
+
+//Spades
+import spades1 from '../images/card_deck_modern/2x/spades-1.png'
+import spades2 from '../images/card_deck_modern/2x/spades-2.png'
+import spades3 from '../images/card_deck_modern/2x/spades-3.png'
+import spades4 from '../images/card_deck_modern/2x/spades-4.png'
+import spades5 from '../images/card_deck_modern/2x/spades-5.png'
+import spades6 from '../images/card_deck_modern/2x/spades-6.png'
+import spades7 from '../images/card_deck_modern/2x/spades-7.png'
+import spades8 from '../images/card_deck_modern/2x/spades-8.png'
+import spades9 from '../images/card_deck_modern/2x/spades-9.png'
+import spades10 from '../images/card_deck_modern/2x/spades-10.png'
+import spades11 from '../images/card_deck_modern/2x/spades-11.png'
+import spades12 from '../images/card_deck_modern/2x/spades-12.png'
+import spades13 from '../images/card_deck_modern/2x/spades-13.png'
+
+//Hearts
+import hearts1 from '../images/card_deck_modern/2x/heart-1.png'
+import hearts2 from '../images/card_deck_modern/2x/heart-2.png'
+import hearts3 from '../images/card_deck_modern/2x/heart-3.png'
+import hearts4 from '../images/card_deck_modern/2x/heart-4.png'
+import hearts5 from '../images/card_deck_modern/2x/heart-5.png'
+import hearts6 from '../images/card_deck_modern/2x/heart-6.png'
+import hearts7 from '../images/card_deck_modern/2x/heart-7.png'
+import hearts8 from '../images/card_deck_modern/2x/heart-8.png'
+import hearts9 from '../images/card_deck_modern/2x/heart-9.png'
+import hearts10 from '../images/card_deck_modern/2x/heart-10.png'
+import hearts11 from '../images/card_deck_modern/2x/heart-11.png'
+import hearts12 from '../images/card_deck_modern/2x/heart-12.png'
+import hearts13 from '../images/card_deck_modern/2x/heart-13.png'
+
+
+
+
+
+// this is the container for ALL of '/dashboard'
+let Clover = [cardBack, clover1, clover2, clover3, clover4, clover5, clover6, clover7, clover8, clover9, clover10, clover11, clover12, clover13]
+let Diamond = [cardBack, diamond1, diamond2, diamond3, diamond4, diamond5, diamond6, diamond7, diamond8, diamond9, diamond10, diamond11, diamond12, diamond13]
+let Spades = [cardBack, spades1, spades2, spades3, spades4, spades5, spades6, spades7, spades8, spades9, spades10, spades11, spades12, spades13]
+let Hearts = [cardBack, hearts1, hearts2, hearts3, hearts4, hearts5, hearts6, hearts7, hearts8, hearts9, hearts10, hearts11, hearts12, hearts13]
+
+
+export {Clover, Diamond, Spades, Hearts}
